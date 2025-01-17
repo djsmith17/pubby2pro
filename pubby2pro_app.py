d0b39a372104716f93a136d47c7b9db5436f234c
Dante Smith <djsmith@bu.edu>
2025-01-16 23:40:49 -0500

    Adding spinner

--- pubby2pro_app.py
+++ pubby2pro_app.py
@@ -1,6 +1,6 @@
 import os
 import streamlit as st
-import pandas
+import pandas as pd
 
 from nba_api_df_org import get_nba_advanced_stats
 
@@ -17,9 +17,11 @@ season_id = '2024-25'
 season_file_name = f'{data_dir}/nba_advanced_stats_{season_id}.csv'
 
 if not os.path.exists(season_file_name):
-    st.write('Dataframe does not exist yet')
-    advance_stats_df = get_nba_advanced_stats(season_id)
+    with st.spinner(text = 'Downloading latest data from nba.com'):
+        advance_stats_df = get_nba_advanced_stats(season_id)
 
-    advance_stats_df.to_csv(season_file_name)
+    advance_stats_df.to_csv(season_file_name, index=True)
 else:
-    st.write('Dataframe does exist')
+    advance_stats_df = pd.read_csv(season_file_name)
+
+st.dataframe(advance_stats_df)
