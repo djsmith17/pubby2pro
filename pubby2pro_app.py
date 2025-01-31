ca8f842acd262b0ee8b30ef53ab0a440eee02ff7
Dante Smith <djsmith@bu.edu>
2025-01-30 23:38:33 -0500

    Adding date time info
    Adding date time info about when the dataset was last update

--- pubby2pro_app.py
+++ pubby2pro_app.py
@@ -38,12 +38,22 @@ if not os.path.exists(season_file_name):
     advance_stats_df.to_csv(season_file_name, index=False)
 else:
     advance_stats_df = pd.read_csv(season_file_name)
+    
+# Get the last modified date of the file
+file_mod_time = os.path.getmtime(season_file_name)
+file_mod_date = datetime.fromtimestamp(file_mod_time).strftime('%b %d, %Y %I:%M %p')
 
 with col3:
     st.write(' ')
     st.write(' ')
     st.write(' ')
     st.write(f'There are {advance_stats_df.shape[0]} active players in the following table')
+
+# Add right-aligned text above the table
+st.markdown(
+    f"<div style='text-align: right; color: grey; font-style: italic;'>This dataset was downloaded on {file_mod_date}</div>",
+    unsafe_allow_html=True
+)
 st.dataframe(advance_stats_df)
 
 st.subheader('Game History')
